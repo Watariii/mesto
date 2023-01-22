be06ca1f8b24307eb44026fd1548ffcdcb68db9e
Nikita Sukhov <veyron-nike@yandex.ru>
2023-01-23 01:26:04 +0300

    feature: add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,120 @@
-let profileButtonInfo = document.querySelector('.profile__button-info');
-let popUp = document.querySelector('.pop-up');
-let popUpCloseIcon = document.querySelector('.pop-up__close-icon');
+const arrayCards = [
+    {
+      name: 'Петропавловск-Камчатский',
+      link: './images/petropavlovs-kamchatskiy-min.jpg'
+    },
+    {
+      name: 'Карелия',
+      link: './images/karelia.jpg'
+    },
+    {
+      name: 'Волга',
+      link: './images/beach-volga.jpg'
+    },
+    {
+      name: 'Домбай',
+      link: './images/dombay.png'
+    }, 
+    {
+      name: 'Гора Эльбрус',
+      link: './images/elbrus.png'
+    },
+    {
+      name: 'Карачаевск',
+      link: './images/karachaevsk-min.jpg'
+    },
+      
+  ];
 
-let profileFirstname = document.querySelector('.profile__firstname');
-let profileJob = document.querySelector('.profile__job');
+const profileButtonInfo = document.querySelector('.profile__button-info');
+const profileButtonCards = document.querySelector('.profile__button-cards')
 
-let popUpFirstname = document.querySelector('.pop-up__input_type_firstname');
-let popUpJob = document.querySelector('.pop-up__input_type_job');
+const popUpInfo = document.querySelector('.pop-up_info');
+const popUpCards = document.querySelector('.pop-up_cards')
 
-let popUpForm = document.querySelector('.pop-up__form');
+const popUpInfoCloseIcon = document.querySelector('.pop-up__close-icon_info');
+const popUpCardsCloseIcon = document.querySelector('.pop-up__close-icon_cards');
 
-function popUpOpened() {
-    popUp.classList.add('pop-up_opened');    
+const profileFirstname = document.querySelector('.profile__firstname');
+const profileJob = document.querySelector('.profile__job');
+
+const popUpFirstname = document.querySelector('.pop-up__input_type_firstname');
+const popUpJob = document.querySelector('.pop-up__input_type_job');
+
+const popUpForm = document.querySelector('.pop-up__form');
+
+const photoElementsList = document.querySelector('.photo-elements__list');
+
+const popUpInfoOpened = () => {
+    popUpInfo.classList.add('pop-up_opened');    
     popUpFirstname.value = profileFirstname.textContent;
     popUpJob.value = profileJob.textContent;
-}
+};
 
-function popUpClose() {
-    popUp.classList.remove('pop-up_opened');
-}
+const popUpInfoClose = () => {
+    popUpInfo.classList.remove('pop-up_opened');
+};
 
-function handleFormSubmit (evt) {
+const handleFormSubmit = evt => {
     evt.preventDefault();
     let changeFirstname = popUpFirstname.value;
     let changeJob = popUpJob.value;
     profileFirstname.textContent = changeFirstname;
     profileJob.textContent = changeJob;
-    popUpClose();
-}
+    popUpInfoClose();
+};
 
-profileButtonInfo.addEventListener('click', popUpOpened);
-popUpCloseIcon.addEventListener('click', popUpClose);
+const popUpCardsOpened = () => {
+    popUpCards.classList.add('pop-up_opened');
+};
+
+const popUpCardsClose = () => {
+    popUpCards.classList.remove('pop-up_opened');
+};
+
+const arrayNameCards = arrayCards.map((item) => {
+    return item.name;
+});
+
+const arrayLinkCards = arrayCards.map((item) => {
+    return item.link;
+});
+
+const createCards = (arrayNameCards, arrayLinkCards) => {
+    const template = `
+        <li>
+        <article class="photo-elements__item">
+        <img
+            src=""
+            alt=""
+            class="photo-elements__capture"
+        />
+        <p class="photo-elements__title"></p>
+        <button class="photo-elements__like" type="button"></button>
+        </article>
+        </li>
+        `;
+    const container = document.createElement('div');
+    container.innerHTML = template;
+    container.querySelector('.photo-elements__title').textContent = arrayNameCards;
+    container.querySelector('.photo-elements__capture').setAttribute('src',arrayLinkCards);
+    container.querySelector('.photo-elements__capture').setAttribute('alt',arrayNameCards);
+
+    return container.firstElementChild;
+};
+
+const addCards = (arrayNameCards, arrayLinkCards) => {
+    photoElementsList.prepend(createCards(arrayNameCards, arrayLinkCards));
+};
+
+for (i=0; i<arrayCards.length; i+=1) {
+    addCards(arrayNameCards[i],arrayLinkCards[i])
+};
+
+profileButtonInfo.addEventListener('click', popUpInfoOpened);
+popUpInfoCloseIcon.addEventListener('click', popUpInfoClose);
+
 popUpForm.addEventListener('submit',handleFormSubmit);
+
+profileButtonCards.addEventListener('click', popUpCardsOpened);
+popUpCardsCloseIcon.addEventListener('click', popUpCardsClose);
